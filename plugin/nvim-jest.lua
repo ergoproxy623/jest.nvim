4b9205b5741aa635b2557fa80f90f35c928e02bf
Arthur Goljanik <48716717+ergoproxy623@users.noreply.github.com>
2025-03-26 10:50:51 +0200

    Update nvim-jest.lua

--- plugin/nvim-jest.lua
+++ plugin/nvim-jest.lua
@@ -8,6 +8,7 @@ require("nvim-jest").setup()
 vim.cmd([[
   command! Jest :call nvim_jest#test_project()
   command! JestFile :call nvim_jest#test_file()
+  command! JestFileCoverage :call nvim_jest#test_file_coverage()
   command! JestSingle :call nvim_jest#test_single()
   command! JestCoverage :call nvim_jest#test_coverage()
 ]])
