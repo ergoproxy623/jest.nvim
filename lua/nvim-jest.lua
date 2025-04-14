64956bc98d3f62c1b86ec1d04a2491943f843d09
Arthur Goljanik <48716717+ergoproxy623@users.noreply.github.com>
2025-04-14 14:30:35 +0300

    Update nvim-jest.lua

--- lua/nvim-jest.lua
+++ lua/nvim-jest.lua
@@ -20,30 +20,31 @@ end
 
 local function get_local_jest()
   local root_dir = vim.fn.finddir("node_modules/..", get_current_folder_path() .. ";")
+  if root_dir == "" then
+    print("ERR: Could not find local node_modules. Is this a JS/TS project?")
+    return "jest"
+  end
   return root_dir .. "/node_modules/jest/bin/jest.js"
 end
 
 local function run_jest(args)
-  local t = {}
-  table.insert(t, "terminal " .. config.jest_cmd)
+  local cmd = { "terminal", config.jest_cmd }
 
   if args ~= nil then
-    for _, v in pairs(args) do
-      table.insert(t, v)
+    for _, v in ipairs(args) do
+      table.insert(cmd, v)
     end
   end
 
-  local jest_cmd = table.concat(t, "")
+  local jest_cmd = table.concat(cmd, " ")
   vim.api.nvim_command(jest_cmd)
 end
 
 function M.setup(user_data)
-  if user_data ~= nil then
-    config.jest_cmd = user_data.jest_cmd or nil
-    config.silent = user_data.silent or nil
-  end
+  config.jest_cmd = user_data and user_data.jest_cmd or nil
+  config.silent = user_data and user_data.silent or nil
 
-  if config.jest_cmd == nil then
+  if not config.jest_cmd or config.jest_cmd == "" then
     config.jest_cmd = get_local_jest()
   end
 
@@ -62,16 +63,17 @@ function M.test_file()
   local c_file = get_current_file_path()
   create_window()
 
-  local args = {}
-  table.insert(args, " --runTestsByPath " .. c_file)
-  table.insert(args, " --watch")
+  local args = {
+    "--runTestsByPath",
+    c_file,
+    "--watch",
+  }
 
   if config.silent then
-    table.insert(args, " --silent")
+    table.insert(args, "--silent")
   end
 
   run_jest(args)
-
   focus_last_accessed_window()
 end
 
@@ -79,17 +81,18 @@ function M.test_file_coverage()
   local c_file = get_current_file_path()
   create_window()
 
-  local args = {}
-  table.insert(args, " --runTestsByPath " .. c_file)
-  table.insert(args, " --watch")
-  table.insert(args, " --coverage")
+  local args = {
+    "--runTestsByPath",
+    c_file,
+    "--watch",
+    "--coverage",
+  }
 
   if config.silent then
-    table.insert(args, " --silent")
+    table.insert(args, "--silent")
   end
 
   run_jest(args)
-
   focus_last_accessed_window()
 end
 
@@ -102,13 +105,15 @@ function M.test_single()
   if test_name ~= nil then
     create_window()
 
-    local args = {}
-    table.insert(args, " --runTestsByPath " .. c_file)
-    table.insert(args, " -t='" .. test_name .. "'")
-    table.insert(args, " --watch")
-    table.insert(args, " --coverage")
-    run_jest(args)
+    local args = {
+      "--runTestsByPath",
+      c_file,
+      "-t=" .. "'" .. test_name .. "'",
+      "--watch",
+      "--coverage",
+    }
 
+    run_jest(args)
     focus_last_accessed_window()
   else
     print("ERR: Could not find test name. Place cursor on line with test name.")
@@ -118,8 +123,9 @@ end
 function M.test_coverage()
   create_window()
 
-  local args = {}
-  table.insert(args, " --coverage")
+  local args = {
+    "--coverage",
+  }
 
   run_jest(args)
   focus_last_accessed_window()
